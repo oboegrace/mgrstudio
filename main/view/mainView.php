6efc596ea3be5f5c6e6ebdd6836fef734196b612
Grace Chien <oboegrace@gmail.com>
2014-03-24 16:40:32 +0800

    edit conflict

--- main/view/mainView.php
+++ main/view/mainView.php
@@ -10,12 +10,7 @@ class mainView extends SiteView {
 		// Vars
 		$templateFolder = $this->getTemplateFolder();
 		$styleFolder 	= $this->getStyleFolder();
-<<<<<<< HEAD
 		$imageFolder	= $this->getImageFolder();
-
-=======
-		$imageFolder 	= $this->getImageFolder();
->>>>>>> grace
 		// Content (XML)
 		$c = $this->model->getContentData();
 
