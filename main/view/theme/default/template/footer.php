2b61674316380922d0bff601d53c11ff3b5d339c
Grace Chien <oboegrace@gmail.com>
2014-03-24 14:31:29 +0800

    slider 還沒改好

--- main/view/theme/default/template/footer.php
+++ main/view/theme/default/template/footer.php
@@ -2,20 +2,27 @@
 	<div class="container">
 		<!-- contact info -->
 		<!-- <div class="sixteen columns"> -->
-			<div class="three columns">
-				<i class="fa fa-phone"></i> 886 (2) 2325-8665
-			</div>
-			<div class="three columns">
-				<i class="fa fa-print"></i> 886 (2) 2325-8655
-			</div>
-			<div class="four columns">
-				<i class="fa fa-envelope-o"></i>
-				<a class="link-mailto" href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=service@mgrstudio.net" target="_blank"> service@mgrstudio.net</a>
-			</div>
-			<div class="five columns">
-				<i class="fa fa-map-marker"></i> 106台北市大安區復興南路一段279巷32號
-			</div>
+		<div class="three columns">
+			<i class="fa fa-phone"></i> 886 (2) 2325-8665
+		</div>
+		<div class="three columns">
+			<i class="fa fa-print"></i> 886 (2) 2325-8655
+		</div>
+		<div class="four columns">
+			<i class="fa fa-envelope-o"></i>
+			<a class="link-mailto" href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=service@mgrstudio.net" target="_blank"> service@mgrstudio.net</a>
+		</div>
+		<div class="five columns">
+			<i class="fa fa-map-marker"></i> 106台北市大安區復興南路一段279巷32號
+		</div>
 
+		<div class="sixteen columns">
+			<div id="footer-logo">
+				<img src="main/view/theme/default/images/mgrLogo-small-white.gif" id="footerlogoImg" />
+			</div>
+			<div id="footer-rights">2011 MARRY GO ROUND MEDIA INC.<br>ALL RIGHTS RESERVED
+			</div>
+		</div>
 		<?=$c->footer_content;?>
 	</div>
 </div>
--- main/view/theme/default/template/index.php
+++ main/view/theme/default/template/index.php
@@ -1,6 +1,71 @@
+<script>
+	$("#slider").on("scroll", function() {
+  		$(".slides").css({
+    	"background-position": $(this).scrollLeft()/6-100+ "px 0"
+  	});  
+	var slider = {
+  
+  	el: {
+    	slider: $("#slider"),
+    	allSlides: $(".slide"),
+    	sliderNav: $(".slider-nav"),
+    	allNavButtons: $(".slider-nav > a")
+  	},
+  	timing: 800,
+  	slideWidth: 300, // could measure this
+
+  	init: function() {
+    	// You can either manually scroll...
+		this.el.slider.on("scroll", function(event) {
+   			slider.moveSlidePosition(event);
+		});
+	    // ... or click a thing
+		this.el.sliderNav.on("click", "a", function(event) {
+			slider.handleNavClick(event, this);
+		});
+	},
+	
+	moveSlidePosition: function(event) {
+	    // Magic Numbers
+		this.el.allSlides.css({
+			"background-position": $(event.target).scrollLeft()/6-100+ "px 0"
+		});  
+  	},
+
+	handleNavClick: function(event, el) {
+		// Don't change URL to a hash, remove if you want that
+		event.preventDefault();
+
+		// Get "1" from "#slide-1", for example
+		var position = $(el).attr("href").split("-").pop();
+		this.el.slider.animate({
+			scrollLeft: position * this.slideWidth
+		}, this.timing);
+
+    this.changeActiveNav(el);
+	},
+	
+	changeActiveNav: function(el) {
+		// Remove active from all links
+		this.el.allNavButtons.removeClass("active");
+		// Add back to the one that was pressed
+		$(el).addClass("active");
+	}
+};
+
+slider.init();
+</script>
+
 <div id="content">
 	<div id="slideshow">
-		slideshow
+		<div class="slider" id="slider">
+			<div class="holder">
+				<div class="slide" id="slide-0"><span class="temp">74°</span></div>
+				<div class="slide" id="slide-1"><span class="temp">64°</span></div>
+				<div class="slide" id="slide-2"><span class="temp">82°</span></div>
+				<div class="slide" id="slide-3"><span class="temp">82°</span></div>
+  			</div>
+		</div>
 	</div>
 	<div id="highlights" >
 		<div class="container">
@@ -19,6 +84,7 @@
 				<h4>title</h4>
 				<p>content</p>
 			</div>
+
 			<!-- <div>
 				<h3><?=$c->welcome_title;?></h3>
 				<p><?=$c->welcome_content;?></p>
@@ -26,3 +92,4 @@
 		</div>
 	</div>
 </div>
+<div class="space-for-footer"></div>
